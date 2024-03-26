15d9078d6d7219ac4bef8fe3f0e57f465aaf3200
Snoulest <pilkatan@gmail.com>
2024-03-26 10:26:31 +0200

    added stack class but still need to check the vals

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/Stack/Stack.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.robotSubSystems.intake;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+public class Stack {
+    private static Servo stackServo;
+    public static void init(HardwareMap hardwareMap) {
+        stackServo = hardwareMap.get(Servo.class, "StackServo");
+    }
+    public static void operate(StackState state){
+        switch (state){
+            case UP:
+                stackServo.setPosition(StackConstants.up);
+                break;
+            case FULL:
+                stackServo.setPosition(StackConstants.full);
+                break;
+            case HALF:
+                stackServo.setPosition(StackConstants.half);
+                break;
+            case BACKBOARD:
+                stackServo.setPosition(StackConstants.backBoard);
+                break;
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/Stack/StackConstants.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.robotSubSystems.intake;
+
+public class StackConstants {
+    public static final double full= 0;
+    public static final double up = 0;
+    public static final double backBoard = 0;
+    public static final double half = 0;
+}
